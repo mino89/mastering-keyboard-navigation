0882e55e374c74ecf340c3d11ed55fba5808477e
pierdomenico.reitano <pierdomenico.reitano@farport.co>
2025-10-04 21:11:35 +0200

    chore:fixed base path

--- vite.config.mjs
+++ vite.config.mjs
@@ -2,7 +2,12 @@ import { defineConfig } from "vite";
 import vituum from "vituum";
 import posthtml from "@vituum/vite-plugin-posthtml";
 import tailwindcss from "@tailwindcss/vite";
+console.log(process.env.NODE_ENV);
 export default defineConfig({
+  base:
+    process.env.NODE_ENV === "production"
+      ? "/mastering-keyboard-navigation/"
+      : "/",
   plugins: [
     tailwindcss(),
     vituum(),
